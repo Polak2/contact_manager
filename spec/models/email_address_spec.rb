3abe4bf28e659a422772f4d419108d51f976f2b2
Polak2 <czesio231@gmail.com>
2016-04-08 21:21:21 +0200

    added tests to email model

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	let(:email_address) do
+		EmailAddress.new(address: '1@1.pl', person_id: 3)
+	end
+
+	it 'is valid' do
+		expect(email_address).to be_valid
+	end
+
+	it 'is invalid without email' do
+		email_address.address = nil
+		expect(email_address).to_not be_valid
+	end
+
 end
